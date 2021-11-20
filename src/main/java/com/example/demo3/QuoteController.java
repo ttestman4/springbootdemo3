36f8aba81bd1ca92f3bc0b1013842dbb15dc7b18
Test Man <ttestman4@gmail.com>
2021-11-20 15:03:37 +0530

    Using web client to make generic proxy for get

--- src/main/java/com/example/demo3/QuoteController.java
+++ src/main/java/com/example/demo3/QuoteController.java
@@ -9,9 +9,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @RestController
 public class QuoteController {
@@ -33,5 +40,46 @@ public class QuoteController {
         Quote result = restTemplate.getForObject(url, Quote.class);
         return result;
     }
-    
+
+    @GetMapping("/quote3")
+    public Object quote3() {
+        String url = "https://quoters.apps.pcfone.io/api/random";
+        return restTemplate.getForObject(url, Object.class);
+    }
+
+    @GetMapping("/quote4/**")
+    public Object quote4(HttpServletRequest request) {
+        WebClient webClient = WebClient.builder().baseUrl("https://quoters.apps.pcfone.io/").build();
+        String requestUrl = request.getRequestURI();
+        return webClient.get().uri(requestUrl.replace("/quote4", "")).retrieve().bodyToMono(Object.class);
+    }
+
+    // @RequestMapping("/**")
+    // public ResponseEntity mirrorRest(@RequestBody(required = false) String body,
+    // HttpMethod method,
+    // HttpServletRequest request, HttpServletResponse response) throws
+    // URISyntaxException {
+    // String requestUrl = request.getRequestURI();
+
+    // URI uri = new URI("http", null, server, port, null, null, null);
+    // uri =
+    // UriComponentsBuilder.fromUri(uri).path(requestUrl).query(request.getQueryString()).build(true).toUri();
+
+    // HttpHeaders headers = new HttpHeaders();
+    // Enumeration<String> headerNames = request.getHeaderNames();
+    // while (headerNames.hasMoreElements()) {
+    // String headerName = headerNames.nextElement();
+    // headers.set(headerName, request.getHeader(headerName));
+    // }
+
+    // HttpEntity<String> httpEntity = new HttpEntity<>(body, headers);
+    // RestTemplate restTemplate = new RestTemplate();
+    // try {
+    // return restTemplate.exchange(uri, method, httpEntity, String.class);
+    // } catch (HttpStatusCodeException e) {
+    // return
+    // ResponseEntity.status(e.getRawStatusCode()).headers(e.getResponseHeaders())
+    // .body(e.getResponseBodyAsString());
+    // }
+    // }
 }
